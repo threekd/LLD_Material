fadc039e82b93d22e84dc0b1e2b5b9601511f802
threekd <threekd42@gmail.com>
2025-05-10 22:18:53 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
-
+  base: '/LLD_Material/',
   title: 'VuePress',
   description: 'My first VuePress Site',
 
