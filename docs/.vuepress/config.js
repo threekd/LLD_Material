61d71045989965c1ddb860c9a6b2f83589fcfbd1
threekd <threekd42@gmail.com>
2025-05-11 02:07:11 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,22 @@
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
-import { mdEnhancePlugin } from "vuepress-plugin-md-enhance";
 
 export default defineUserConfig({
   lang: 'en-US',
   base: '/LLD_Material/',
-  title: 'New Purchase & Inventory',
-  description: 'Low Level Design Document',
+  title: 'VuePress',
+  description: 'My first VuePress Site',
 
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
 
     navbar: [
-      '/', 
-      '/flowchart',
+      '/',
       '/document',
+      '/flowchart'
     ],
   }),
 
-  plugins: [
-    mdEnhancePlugin({
-      // 启用流程图
-      flowchart: true,
-    }),
-  ],
-
   bundler: viteBundler(),
 })
-
--- docs/flowchart.md
+++ docs/flowchart.md
@@ -1,11 +1,15 @@
-# Flowchart
+# Flowchart for Procurement Process
 
-```flowchart
-st=>start: 开始
-e=>end: 结束
-op=>operation: 操作
-cond=>condition: 条件
+```mermaid
 
-st->op->cond
-cond(yes)->e
-cond(no)->op
+graph TD
+    st([Start]) --> add_request[请购员提交采购申请]
+    add_request --> purchase_item[采购明细]
+    purchase_item --> add_purchase_order[生成采购单]
+    add_purchase_order --> create_PO[下订单并确认采购]
+    create_PO --> supplier_confirm[供应商确认订单并发货]
+    supplier_confirm --> receive_check{采购人员收到货品并检查}
+    receive_check -->|是| good_category{是否为一般消耗品?}
+    receive_check -->|否| good_exchange[联系供应商退换货] --> supplier_confirm
+    good_category -->|是| buyer_gr[采购人员入库] --> process_end([Process End])
+    good_category -->|否| user_gr[请购员入库] --> process_end([Process End])
